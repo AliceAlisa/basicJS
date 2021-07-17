1563d84b7952c161b16b585cfe1631975421cdef
alicew <wanderlust.alice00@gmail.com>
2021-07-17 18:49:41 +0200

    task 2 started in HW 4

--- lesson4/index.js
+++ lesson4/index.js
@@ -1,5 +1,5 @@
 'use strict';
-/*Задание 1 */
+/*Задание 1 
 
 function makeObj(number) {
     if (number > 999) {
@@ -18,8 +18,49 @@ function makeObj(number) {
 }
 
 console.log(makeObj(481));
+*/
 /*const firstObj = makeObj(481);
 console.log(firstObj);  Не совсем поняла, обязательно ли было присваивать объекту константу/переменную, или просто вывести в консоль результат работы функции? */
 
-/*Задание 2 */
 
+
+/*Задание 2 */
+const cart = {
+    productList: [
+        ['jeans', 50, 1],
+        ['T-shirt', 15, 3],
+        ['shorts', 20, 2],
+        ['cap', 15, 2],
+        ['sneakers', 80, 1]
+    ],
+    countBasketPrice(cart) {
+        let summ = 0;
+        for (let i = 0; i < cart.length; i++) {
+            summ += cart[i][1] * cart[i][2];
+        }
+        return summ;
+    }
+}
+console.log('Сумма корзины: ' + cart.countBasketPrice(cart.productList) + '\n');
+
+/*
+function countBasketPrice(cart) {
+    let summ = 0;
+    for (let i = 0; i < cart.length; i++) {
+        summ += cart[i][1] * cart[i][2];
+    }
+    return summ;
+}
+
+alert('Стоимость корзины составляет: ' + countBasketPrice(cart));
+
+
+
+let cartalt = [
+    ['jeans', 50, 1],
+    ['T-shirt', 15, 3],
+    ['shorts', 20, 2],
+    ['cap', 15, 2],
+    ['sneakers', 80, 1]
+];
+*/
