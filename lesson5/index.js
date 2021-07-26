c3281df47576bee4accd6a02bd47de9e4c057e94
alicew <wanderlust.alice00@gmail.com>
2021-07-26 22:23:13 +0200

    HW5 completed

--- lesson5/index.js
+++ lesson5/index.js
@@ -38,8 +38,7 @@ const chessBoard = {
 };
 chessBoard.initChessCells();
 
-
-/*Задание 2 */
+/*Задание 2*/
 let jeans = {};
 let tshirt = {};
 let shorts = {};
@@ -83,51 +82,71 @@ const cart = {
     checkCart() {
         if (this.productList.length == 0) {
             this.containerCart.textContent = 'Корзина пуста!';
+            return this.containerCart.textContent;
         } else {
             this.containerCart.textContent = 'В корзине ' + this.productList.length + ' товаров ' + 'на сумму ' + this.countBasketPrice() + ' рублей';
+            return this.containerCart.textContent;
         }
     }
 }
 cart.checkCart();
 
-/*Задание 3 
-let jeans = {};
-let tshirt = {};
-let shorts = {};
-let cap = {};
-let sneakers = {};
-*/
+/*Задание 3 */
+let j33444 = {
+    name: 'jeans',
+    price: 50,
+};
+let t56737 = {
+    name: 'T-shirt',
+    price: 15,
+};
+let s80904 = {
+    name: 'shorts',
+    price: 20,
+};
+let c76833 = {
+    name: 'cap',
+    price: 15,
+};
+let s54521 = {
+    name: 'sneakers',
+    price: 80,
+};
+
 let product = {
+
     productList: [
-        jeans = {
-            price: 50,
-            quantity: 1
-        },
-        tshirt = {
-            price: 15,
-            quantity: 3
-        },
-        shorts = {
-            price: 20,
-            quantity: 2
-        },
-        cap = {
-            price: 15,
-            quantity: 2
-        },
-        sneakers = {
-            price: 80,
-            quantity: 1
-        }
+        j33444,
+        t56737,
+        s80904,
+        c76833,
+        s54521
     ],
 
     containerCatalog: document.getElementById('catalog'),
+    productArr: [],
+    totalHTML: '',
+
+    generateProductArr() {
+        for (let i = 0; i < this.productList.length; i++) {
+            this.productArr.push(this.productList[i].name);
+            this.productArr.push(this.productList[i].price);
+        }
+        return this.productArr;
+    },
 
     displayProduct() {
-        for (let i = 0; i < this.productList.length; i++) {
-            let productNext = this.productList[i];
-            this.containerCatalog.textContent += '\n' + this.productList[i] + Object.keys(productNext);
-        }
+        this.containerCatalog.innerHTML = '<p> Каталог </p>';
+        this.generateProductArr();
+        for (let i = 0; i < this.productArr.length; i++) {
+            this.totalHTML += this.productArr[i] + ' ';
+            if (i % 2 == 1) {
+                this.totalHTML += ' rub <br>'
+            } else {
+                this.totalHTML += ': '
+            }
+        };
+        this.containerCatalog.innerHTML += this.totalHTML;
     }
 }
 
